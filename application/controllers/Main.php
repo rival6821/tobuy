f20c5585e7b2e202b214aaeb5014402bf67fa078
ilhoon lee <rival6821@naver.com>
2018-12-29 20:28:14 +0900

    scss

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -10,7 +10,9 @@ class Main extends CI_Controller {
 
 	public function index()
 	{	
+		$this->load->view('template/header');
 		$this->load->view('main/main');
+		$this->load->view('template/footer');
 	}
 
 }
